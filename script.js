0cd1e6aa4fca25ee149dd4726df7825f5ead3420
Praveen Sankhla <pk54451636465@gmail.com>
2022-04-05 18:16:56 +0530

    Hey Pk!

--- script.js
+++ script.js
@@ -8,3 +8,21 @@ setInterval(
     document.getElementById("searchtab").addEventListener("click",()=>{
    document.getElementById("searchinput").style.display="block";
   })
+
+
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("logindiv");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+    document.getElementsByClassName("logindiv").style.margin = "10px 0px";
+    document.getElementById("navlogo").src = "https://cdn.shopify.com/s/files/1/2090/3151/files/Daily_Horoscope_100x100.png?v=1637837610";
+  } else {
+    navbar.classList.remove("sticky");
+    document.getElementsByClassName("logindiv").style.margin = "20px 0px";
+    document.getElementById("navlogo").src = "https://cdn.shopify.com/s/files/1/2090/3151/t/193/assets/logo-optimized.svg?v=5427892325386485898";
+  }
+}
